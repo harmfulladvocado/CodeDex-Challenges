e54911d923a2e3033a7f265f4e71f8c5fd749799
HarmfullAdvocado <214548733+harmfulladvocado@users.noreply.github.com>
2026-03-02 00:05:18 +0100

    Update 5.Fitness Routine.js

--- Javascript/1.Conditionals/5.Fitness Routine.js
+++ Javascript/1.Conditionals/5.Fitness Routine.js
@@ -1,14 +1,11 @@
-Instructions
-Exercising is essential for our health and quality of life. Especially for devs! With so many types of workouts available, taking the first step can feel intimidating.
+let randomNumber = Math.floor(Math.random() * 4);
 
-Why don't we use conditionals and random numbers to help us pick an exercise and make it fun?
-
-Create a randomNumber variable that generates a random number between 0 and 3.
-
-Use a conditional statement to log one of these:
-
-0: "10 Push-ups"
-1: "10 Sit-ups"
-2: "10 Squats"
-3: "10 Jumping Jacks"
-Now, do the fitness challenge for real! 🔥
+if (randomNumber === 0) {
+  console.log("10 Push-ups");
+} else if (randomNumber === 1) {
+  console.log("10 Sit-ups");
+} else if (randomNumber === 2) {
+  console.log("10 Squats");
+} else {
+  console.log("10 Jumping Jacks");
+}
